9ddf8a4b5f5773563d767b7c525bd9f091618836
Vinícius Gabriel <149544283+viniciusgcf@users.noreply.github.com>
2023-12-18 08:02:23 -0300

    Update App.js

--- resources/App.js
+++ resources/App.js
@@ -1,42 +1,20 @@
-import { Text, SafeAreaView, StyleSheet, Image } from 'react-native';
-
-
+import { Text, SafeAreaView, StyleSheet, Image, StatusBar } from 'react-native';
 import { Card } from 'react-native-paper';
+import Login from './resources/inicio'
 
-
-export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Card>
-        <Image style={styles.logo} source={require('/../assets/logoGLPI.png')} />
-          <Text style={styles.paragraph}>
-            Insira seu login de acesso
-          </Text>
-      </Card>
-
-    </SafeAreaView>
-  );
+export default function Background(){
+  return <>
+    <Card>
+    <Image style={styles.logo} source={require('/assets/logoGLPI.png')} />
+    </Card>
+    <Login/>
+  </>
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-    padding: 8,
+logo: {
+  marginLeft: 40,
+  height: 138,
+  width: 250,
   },
-    logo: {
-    marginTop: 70,
-    height: 128,
-    width: 300,
-  },
-  paragraph: {
-    margin: 24,
-    marginTop: 100,
-    fontSize: 14,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
-
-
+})
